9e16f90b3a4f45c1dfdf4931cab7c06a77fad9d8
Kamiel de Visser <kamieldevisser@gmail.com>
2025-04-25 14:35:26 +0200

    add basic skeleton

--- my-web-portal-app/src/routes/+layout.svelte
+++ my-web-portal-app/src/routes/+layout.svelte
@@ -4,4 +4,14 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex min-h-screen flex-col">
+	<nav class="h-24">
+		<a href="/">Home</a>
+		<a href="/dashboard">Dashboard</a>
+		<a href="/about">About</a>
+	</nav>
+	<main class="flex-grow">
+		{@render children()}
+	</main>
+	<footer class="h-24">Footer</footer>
+</div>
