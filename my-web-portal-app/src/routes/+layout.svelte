5ebf61ff3265ce35d909b1e1e31cef65218082e3
Kamiel de Visser <kamieldevisser@gmail.com>
2025-04-28 13:28:10 +0200

    add margin
    add margin

--- my-web-portal-app/src/routes/+layout.svelte
+++ my-web-portal-app/src/routes/+layout.svelte
@@ -7,8 +7,10 @@
 
 <div class="flex min-h-screen flex-col">
 	<NavBar />
-	<main class="flex-grow">
-		{@render children()}
+	<main class="flex flex-grow justify-center py-4">
+		<div class="max-w-[min(85%,100ch)] flex-grow">
+			{@render children()}
+		</div>
 	</main>
 	<footer class="h-24">Footer</footer>
 </div>
