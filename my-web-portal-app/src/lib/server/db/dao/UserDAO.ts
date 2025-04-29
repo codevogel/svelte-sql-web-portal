08063fa5072a7c8bb0ff7868e89b5057f9d6bd7d
Kamiel de Visser <kamieldevisser@gmail.com>
2025-04-29 18:21:26 +0200

    add UserDAO

---
+++ my-web-portal-app/src/lib/server/db/dao/UserDAO.ts
@@ -0,0 +1,11 @@
+import { db } from '$lib/server/db';
+import { user, type User } from '$lib/server/db/schema';
+
+export class UserDAO {
+	// Get all users from the database
+	static async getAll(): Promise<User[]> {
+		// Drizzle ORM query to select all users
+		// Note the similarity to the SQL query: SELECT * FROM user
+		return await db.select().from(user);
+	}
+}
