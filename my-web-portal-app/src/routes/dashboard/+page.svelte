6d582a374c80f06309f5f0a31f24e47f739a8171
Kamiel de Visser <kamieldevisser@gmail.com>
2025-04-29 18:21:50 +0200

    add display data from server

--- my-web-portal-app/src/routes/dashboard/+page.svelte
+++ my-web-portal-app/src/routes/dashboard/+page.svelte
@@ -1,1 +1,6 @@
-Welcome to my dashboard!
+<script lang="ts">
+	import UserTable from '$lib/components/ui/views/tables/UserTable.svelte';
+	const { data } = $props();
+</script>
+
+<UserTable users={data.users} />
