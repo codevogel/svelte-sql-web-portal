d50867a8c88c936bc8a3ccc34288e139d269315e
Kamiel de Visser <kamieldevisser@gmail.com>
2025-04-29 18:21:18 +0200

    add UserTable

---
+++ my-web-portal-app/src/lib/components/ui/views/tables/UserTable.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { User } from '$lib/server/db/schema';
+
+	let { users }: { users: User[] } = $props();
+</script>
+
+<div class="table-wrap">
+	<table class="table caption-bottom">
+		<caption class="pt-4">A list of users and their age.</caption>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Age</th>
+			</tr>
+		</thead>
+		<tbody class="[&>tr]:hover:preset-tonal-primary">
+			{#each users as user (user.id)}
+				<tr>
+					<td>{user.name}</td>
+					<td>{user.age}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
