f9bc36025f7bdc39305dcb36aa1401dd327159e5
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-02 18:00:31 +0200

    formatting

--- my-web-portal-app/src/lib/server/db/dao/UserDAO.ts
+++ my-web-portal-app/src/lib/server/db/dao/UserDAO.ts
@@ -1,11 +1,15 @@
 import { db } from '$lib/server/db';
 import { user, type User } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 
 export class UserDAO {
 	// Get all users from the database
 	static async getAll(): Promise<User[]> {
 		// Drizzle ORM query to select all users
 		// Note the similarity to the SQL query: SELECT * FROM user
-		return await db.select().from(user);
+		return await db
+			.select()
+			.from(user);
+	}
 	}
 }
