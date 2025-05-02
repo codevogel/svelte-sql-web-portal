034112292430b2b49e0dadae4454462381633c50
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-02 18:03:10 +0200

    display top score table

--- my-web-portal-app/src/routes/dashboard/+page.server.ts
+++ my-web-portal-app/src/routes/dashboard/+page.server.ts
@@ -1,12 +1,15 @@
 import type { User } from '$lib/server/db/schema';
 import type { PageServerLoad } from './$types';
 import { UserDAO } from '$lib/server/db/dao/UserDAO';
+import { ScoreDAO, TopScore } from '$lib/server/db/dao/ScoreDAO';
 
 export const load: PageServerLoad = async () => {
 	// Here we query the user data using our DAO and return it
 	const users: User[] = await UserDAO.getAll();
+	const topScores: TopScore[] = await ScoreDAO.getTopScorers();
 	return {
 		users,
+		topScores
 	};
 
 }
--- my-web-portal-app/src/routes/dashboard/+page.svelte
+++ my-web-portal-app/src/routes/dashboard/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import TopScoreTable from '$lib/components/ui/views/tables/TopScoreTable.svelte';
 	import UserTable from '$lib/components/ui/views/tables/UserTable.svelte';
 	const { data } = $props();
 </script>
 
 <UserTable users={data.users} />
+<TopScoreTable topScores={data.topScores} />
