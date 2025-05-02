eba98dc033410019a816ca1b5d30e9dbbb6efedc
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-02 18:03:43 +0200

    add SessionView

---
+++ my-web-portal-app/src/lib/components/ui/views/SessionView.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Score, Session, User } from '$lib/server/db/schema';
+	import ScoreTable from '$lib/components/ui/views/tables/ScoreTable.svelte';
+
+	let { session, user, scores }: { session: Session; user: User; scores: Score[] } = $props();
+
+	let sessionEnd = new Date(session.createdAt.getTime() + session.duration * 1000);
+</script>
+
+<h1>Session {session.id}</h1>
+<p><span class="font-bold">User name:</span> {user.name}</p>
+<p>Session started: {session.createdAt.toLocaleString()}</p>
+<p>Session end: {sessionEnd.toLocaleString()}</p>
+
+<h1>Scores in this session</h1>
+<ScoreTable {scores} />
