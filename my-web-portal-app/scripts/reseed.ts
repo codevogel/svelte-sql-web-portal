2f644b1cccad2219e498d6fe1d25ca19facaab0e
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-02 17:58:36 +0200

    ensure score.createdAt seed during session

--- my-web-portal-app/scripts/reseed.ts
+++ my-web-portal-app/scripts/reseed.ts
@@ -1,14 +1,19 @@
 import { reset, seed } from "drizzle-seed";
-import { drizzle } from "drizzle-orm/mysql2";
+import { drizzle, MySql2Database } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import *  as schema from "../src/lib/server/db/schema";
 import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import { eq } from "drizzle-orm";
 
 async function reseed_db() {
 	if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 	const client = mysql.createPool(process.env.DATABASE_URL);
 	const db = drizzle(client, { schema, mode: 'default' });
+
+	faker.seed(1234);
+
 	console.log("Resetting database...");
 	await reset(db, schema);
 	console.log("Reseeding database...");
@@ -24,7 +29,7 @@ async function reseed_db() {
 		session: {
 			count: 100,
 			columns: {
-				startedAt: f.date({ minDate: "2025-02-01", maxDate: "2025-03-01" }),
+				createdAt: f.date({ minDate: "2025-02-01", maxDate: "2025-03-01" }),
 				duration: f.int({ minValue: 0, maxValue: 2 * 60 * 60 }),
 			},
 		},
@@ -45,12 +50,34 @@ async function reseed_db() {
 		}
 	}));
 
+	await setScoreCreatedAtAfterSessionCreatedAt(db);
+
 	console.log("Database reseeded successfully");
 	await client.end();
 }
 
 async function main() {
 	await reseed_db();
+}
+
+async function setScoreCreatedAtAfterSessionCreatedAt(db: MySql2Database<typeof schema>) {
+	const sessions = await db.select().from(schema.session);
+	for (const session of sessions) {
+		const sessionStart = session.createdAt;
+		const sessionEnd = new Date(sessionStart.getTime() + session.duration * 1000);
+		const sessionScores = await db.query.score.findMany({
+			where: eq(schema.score.sessionId, session.id),
+		});
+
+		for (const score of sessionScores) {
+			await db.update(schema.score).set({
+				createdAt: faker.date.between({
+					from: sessionStart,
+					to: sessionEnd,
+				}),
+			}).where(eq(schema.score.id, score.id));
+		}
+	}
 }
 
 main()
