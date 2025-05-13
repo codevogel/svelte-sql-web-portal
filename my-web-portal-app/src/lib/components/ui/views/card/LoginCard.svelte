0ace30da61ce7f85ccdeee7586dced6eff516660
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-13 11:02:57 +0200

    add LoginCard

---
+++ my-web-portal-app/src/lib/components/ui/views/card/LoginCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Card from '$lib/components/ui/views/card/Card.svelte';
+</script>
+
+<Card footerBase="flex flex-col p-8 gap-y-4">
+	{#snippet header()}
+		<h1 class="h1 p-8">Log In</h1>
+	{/snippet}
+	{#snippet article()}
+		<form class="mx-auto w-full max-w-md space-y-4">
+			<!-- --- -->
+			<label class="label">
+				<span class="label-text">Name</span>
+				<input type="text" class="input" placeholder="Enter name" />
+			</label>
+			<!-- --- -->
+			<label class="label">
+				<span class="label-text">Password</span>
+				<input type="password" class="input" placeholder="Enter Password" />
+			</label>
+		</form>
+	{/snippet}
+	{#snippet footer()}
+		<button class="btn preset-filled-primary-500">Log In</button>
+		<small>
+			Don't have an account? <a href="/register" class="text-primary-500">Register</a>
+		</small>
+	{/snippet}
+</Card>
