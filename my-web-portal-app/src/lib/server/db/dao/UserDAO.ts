11a5fee3216b48fabb4083601c7e088e0870be39
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-13 11:05:03 +0200

    add user queries

--- my-web-portal-app/src/lib/server/db/dao/UserDAO.ts
+++ my-web-portal-app/src/lib/server/db/dao/UserDAO.ts
@@ -1,28 +1,29 @@
 import { db } from '$lib/server/db';
 import { user, type User } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm';
+import { eq, like } from 'drizzle-orm';
 
 export class UserDAO {
 	// Get all users from the database
 	static async getAll(): Promise<User[]> {
 		// Drizzle ORM query to select all users
 		// Note the similarity to the SQL query: SELECT * FROM user
-		return await db
-			.select()
-			.from(user);
+		return await db.select().from(user);
 	}
 
 	static async getUserById(id: number): Promise<User> {
-		const result: User | undefined = await db
-			.query
-			.user
-			.findFirst({
-				where: eq(user.id, id)
-			});
+		const result: User | undefined = await db.query.user.findFirst({
+			where: eq(user.id, id)
+		});
 		if (!result) {
 			throw new Error(`User with id ${id} not found`);
 		}
 		return result;
 	}
-}
 
+	static async getUsersLikeName(name: string): Promise<User[]> {
+		const result: User[] = await db.query.user.findMany({
+			where: like(user.name, `%${name}%`)
+		});
+		return result;
+	}
+}
