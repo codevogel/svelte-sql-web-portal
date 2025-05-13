15aa45c2d50215d8c59cc14e2e9529d91b63567b
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-13 11:40:39 +0200

    update dashboard landing page

--- my-web-portal-app/src/routes/dashboard/+page.svelte
+++ my-web-portal-app/src/routes/dashboard/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+	import Card from '$lib/components/ui/views/card/Card.svelte';
 	import TopScoreTable from '$lib/components/ui/views/tables/TopScoreTable.svelte';
 	import UserTable from '$lib/components/ui/views/tables/UserTable.svelte';
 	const { data } = $props();
 </script>
 
-<UserTable users={data.users} />
-<TopScoreTable topScores={data.topScores} />
+<div class="mx-auto grid grid-cols-1 gap-y-4">
+	<Card baseExtension="!max-w-full w-6xl">
+		{#snippet header()}
+			<h1>Top 10 Users</h1>
+		{/snippet}
+		{#snippet article()}
+			<TopScoreTable topScores={data.topScores} />
+		{/snippet}
+	</Card>
+	<Card baseExtension="!max-w-full w-6xl">
+		{#snippet header()}
+			<h1>Users</h1>
+		{/snippet}
+		{#snippet article()}
+			<UserTable users={data.users} />
+		{/snippet}
+	</Card>
+</div>
