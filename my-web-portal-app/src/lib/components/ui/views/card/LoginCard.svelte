f966a88c215747dbb2ef45d4bd381a8d934331db
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-21 15:07:52 +0300

    use github icon

--- my-web-portal-app/src/lib/components/ui/views/card/LoginCard.svelte
+++ my-web-portal-app/src/lib/components/ui/views/card/LoginCard.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import Card from '$lib/components/ui/views/card/Card.svelte';
+	import GitHubIcon from '$lib/components/ui/logo/GitHubIcon.svelte';
 </script>
 
 <Card footerBase="flex flex-col p-8 gap-y-4">
 	{#snippet header()}
-		<h1 class="h1 p-8">Log In</h1>
+		<h1 class="h2">Log In</h1>
 	{/snippet}
 	{#snippet article()}
 		<button class="btn preset-filled-primary-500" onclick={() => goto('/login/github')}
-			>Log in with GitHub</button
+			><span><GitHubIcon /></span>Log in with GitHub</button
 		>
+		<p>
+			Please note that your GitHub account has to be whitelisted by the site administrator in order
+			to access this website.
+		</p>
 	{/snippet}
 </Card>
