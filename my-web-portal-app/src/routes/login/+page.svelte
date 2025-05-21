7524592b89db38501f4e27a4bb0feac8fceec78f
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-21 15:14:04 +0300

    formatting, redirect from login when logged in

--- my-web-portal-app/src/routes/login/+page.svelte
+++ my-web-portal-app/src/routes/login/+page.svelte
@@ -2,8 +2,6 @@
 	import LoginCard from '$lib/components/ui/views/card/LoginCard.svelte';
 </script>
 
-<div
-	class="grid h-full w-full grid-cols-1 items-center justify-items-center gap-y-8 lg:grid-cols-2"
->
+<div class="grid h-full w-full grid-cols-1 items-center justify-items-center gap-y-8 px-4">
 	<LoginCard />
 </div>
